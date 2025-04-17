1798273f00f968a0b4b7db0dfaf62ab175d705ef
Abeeshan Sridaran <105498915+beemozzy221@users.noreply.github.com>
2025-04-18 00:52:48 +0530

    AC

--- profeatandlabelsencoder.py
+++ profeatandlabelsencoder.py
@@ -90,6 +90,8 @@ def modified_ready_encoding(no_seq, protein_length, aminochars):
     # Parse the FASTA files
     fasta_sequences = SeqIO.parse(open(input_file), 'fasta')
     for no, fasta in enumerate(fasta_sequences):
+        print(f"Now processing for sequence {no}")
+        
         pro_id, sequence = fasta.id, str(fasta.seq)
         panther_id = fasta.description.split(maxsplit=1)[1]
 
